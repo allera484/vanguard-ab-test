191a55ed27ebbf1e325fa8f216162a2febf914b0
Alex Ortiz <alexortizcotto0@gmail.com>
2024-01-09 20:44:22 -0400

    Modified

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # vanguard-ab-test
+
+  Project status(Active, On-Hold, Completed)
+# Project objective
+
+  Paragraph with introduction/ objective of project
+# Methods
+
+  List with methods, ex:
+  - Filtering
+  - Grouping
+  - Visualization
+# Technologies 
+
+  List with used technologies, ex:
+  - Python
+  - Pandas
+  - MySQL
+# Project Description
+
+  Paragraph with a description of the dataset, sources, characteristics ,etc.
+
+# Steps
+  Add here any insights you had during the project
+
+  Structure:
+vanguard-ab-test/
+├── config.yaml
+├── README.md
+├── requirements.txt
+├── data/
+│   └── raw/
+│   │    ├── df_final_demo.txt
+│   │    ├── df_final_experiment_clients.txt
+│   │    ├── df_final_web_data_pt_1.txt
+│   │    └── df_final_web_data_pt_2.txt
+│   └── sql/
+│        └── ab_test_schema.sql
+├── notebook/
+│   └── ab_import.ipynb
+└── src/
+    └── functions/
+        └── ab_clean.py
+
+# Conclusion
+  Final conclusion
+  
+# Contact
+  linkedin, github, medium, etc 
+  
---
+++ src/functions/ab_clean.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+def read_csv(file_path):
+    ''' Reads a CSV file and returns a pandas DataFrame.
+    Usage: This function only takes as parameters the path of the file.
+    Return: Dataframe of the CSV.
+    '''
+    return pd.read_csv(file_path)
+
+def connect_to_db(user, password, database_name):
+    ''' Establishes a connection to the MySQL database and returns an engine object using SQLAlchemy with the provided password and database name.
+    Usage: This function takes three parameters the user name, the password to MySQL and the name of the database your going to insert the data.
+    Return: Engine object that is connected to the database.
+    '''
+    connection_string = f'mysql+pymysql://{user}:' + password + '@localhost/' + database_name
+    return create_engine(connection_string)
+
+def insert_to_table(data, engine, table_name):
+    '''Inserts data into the specified table in the database using SQLAlchemy 'to_sql()' method.
+    Usage: This function takes three parameters the data to be inserted, the engine that was created and the name of the table your going to insert the data.
+    Return: The number of row of data inserted.
+    '''
+    data.to_sql(table_name, con=engine, if_exists='append', index=False)
+
+def filter_data(data, unique_client_ids):
+    '''Filters data based on unique_client_ids (list/array) and returns the filtered DataFrame.
+    Usage: This function takes two parameters the data to be analyze and the list of unique client_ids.
+    Return: Dataframe that only contains the client_ids given.
+    '''
+    return data[data['client_id'].isin(unique_client_ids)]
+
+def reset_index(data):
+    '''Resets the index and renames the index column to 'id' in the DataFrame.
+    Usage: This function only takes one paramether which is the dataset given.
+    Return: Dataframe that contains a new column called id.
+    '''
+    data.reset_index(inplace=True)
+    data.rename(columns={'index': 'id'}, inplace=True)
